a7f158cba550860bad91cf21476c2450605bed2f
Ivanov Peter <petara94@mail.ru>
2021-12-22 22:39:42 +0300

    linux tests stdin test added

--- tests/linux_test.go
+++ tests/linux_test.go
@@ -129,3 +129,27 @@ func TestHttp4(t *testing.T) {
 
 	assert.Equal(t, w.Code, 400)
 }
+
+func TestHttp5(t *testing.T) {
+	executer.StartMachine()
+	r := route.InitRouter()
+
+	w := httptest.NewRecorder()
+
+	Rb := executer.RequestBody{
+		CMD:   "../stdin_test_prog/test_stdin",
+		OS:    "linux",
+		Stdin: "2 2\n",
+	}
+
+	bodyBytes, err := json.Marshal(Rb)
+	body := bytes.NewReader(bodyBytes)
+
+	assert.Nil(t, err)
+	req, err := http.NewRequest("GET", "/api/v1/exec", body)
+
+	assert.Nil(t, err)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, w.Body.String(), "{\"stdout\":\"a + a * b = 6\\n\",\"stderr\":\"\"}")
+}
